a80246ebbd27e78d6b1306c6fabd40361aaa90d0
KoheiNishino <koheinishino.95@gmail.com>
2021-05-05 10:19:29 +0900

    5. How to create Reducers and use the spread syntax.

---
+++ src/reducks/users/reducers.js
@@ -0,0 +1,18 @@
+import * as Actions from "./actions";
+import initialState from "../store/initialState";
+
+export const UserReducer = (state = initialState.users, action) => {
+  switch (action.type) {
+    case Actions.SIGN_IN:
+      // 1. 変更前のstateの状態を展開
+      // 2. spread構文を使用してaction.payloadの値でstateの状態を上書きする
+      // ※ `...state`がないとstateにあってaction.payloadに存在しない値がある場合、
+      //    Mergeされた際にstateの値が消えてしまう
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
