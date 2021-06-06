fb391de736e291f1314548f152026626d350dd60
KoheiNishino <koheinishino.95@gmail.com>
2021-06-06 15:52:24 +0900

    9. Create a native app-style header.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import { useDispatch, useSelector } from "react-redux";
+import { getIsSignedIn } from "../../reducks/users/selectors";
+import logo from "../../assets/img/icons/logo.png";
+import { HeaderMenu } from "./index";
+import { push } from "connected-react-router";
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuBar: {
+      backgroundColor: "#fff",
+      color: "#444",
+    },
+    toolbar: {
+      margin: "0 auto",
+      maxWidth: 1024,
+      width: "100%",
+    },
+    iconButtons: {
+      margin: "0 0 0 auto",
+    },
+  });
+
+const Header = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  const isSignedIn = getIsSignedIn(selector);
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.menuBar}>
+        <Toolbar className={classes.toolbar}>
+          <img
+            alt="Logo"
+            src={logo}
+            width="128px"
+            onClick={() => dispatch(push("/"))}
+            role="button"
+          />
+          {isSignedIn && (
+            <div className={classes.iconButtons}>
+              <HeaderMenu />
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderMenu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { Badge } from "@material-ui/core";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const HeaderMenu = (props) => {
+  return (
+    <>
+      <IconButton>
+        <Badge badgeContent={3} color="secondary">
+          <ShoppingCartIcon />
+        </Badge>
+      </IconButton>
+      <IconButton>
+        <FavoriteBorderIcon />
+      </IconButton>
+      <IconButton
+        aria-label="Menu Items"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        color="inherit"
+      >
+        <MenuIcon />
+      </IconButton>
+    </>
+  );
+};
+
+export default HeaderMenu;
